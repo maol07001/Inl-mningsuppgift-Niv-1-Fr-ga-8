7e01269db320514c4b21739d9220fa747b9e3a1a
mariu <mariu@DESKTOP-CT70FK7>
2024-12-16 16:54:22 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1 Fråga 8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int produkt = 1; 
+            for (int i = 1; i <= 5; i++)
+            {
+                produkt *= i; // produkt = produkt * i
+            }
+
+            Console.WriteLine("Produkten av heltalen från 1 till 5 är:" + produkt);
+        }
+    }
+}
